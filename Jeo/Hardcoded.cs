eec47e65ed315e9022a43d103a2aba4168442251
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2022-05-23 11:32:25 +0200

    Create 2D Array

---
+++ Jeo/Hardcoded.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeo
+{
+    public class Hardcoded
+    {
+        private double[,] arr;
+
+        public Hardcoded()
+        {
+            arr = new double[3, 3]
+            {
+                { 2, 7, 1 }, { 3, -2, 8 }, {1, 5, 3}
+            };
+        }
+
+        public void printArr()
+        {
+            Console.WriteLine(arr);
+        }
+    }
+}
