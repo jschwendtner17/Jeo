b44db347ca74a49126f45daecfcedba158e6e235
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2022-06-13 11:37:41 +0200

    User input

--- Jeo/Program.cs
+++ Jeo/Program.cs
@@ -2,10 +2,24 @@
 
 Hardcoded h = new Hardcoded();
 
-var arrC = new double[3, 3]
+const int MATRIX_ROWS = 3;
+const int MATRIX_COLUMNS = 3;
+
+double[,] arrC = new double[MATRIX_ROWS, MATRIX_COLUMNS];
+
+for (int i = 0; i < MATRIX_ROWS; i++)
+{
+    for (int j = 0; j < MATRIX_COLUMNS; j++)
     {
-        { 2, 7, 1 }, { 3, -2, 8 }, {1, 5, 3}
-    };
+        double input;
+        Console.Write("Enter value for ({0},{1}): ", i, j);
+        while (!double.TryParse(Console.ReadLine(), out input))
+        {
+            Console.Write("Enter correct value for ({0},{1}): ", i, j);
+        }
+        arrC[i, j] = input;
+    }
+}
 
 h.calcInputs(arrC);
 
