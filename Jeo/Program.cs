7254bdb093d39c19b73eca31b29df9f483d1c18a
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2022-06-20 11:42:31 +0200

    Constant matrix for testing

--- Jeo/Program.cs
+++ Jeo/Program.cs
@@ -1,28 +1,28 @@
 ﻿using Jeo;
 
-Console.Write("Matrix size: ");
-int MATRIX_ROWS_COLUMNS = Int32.Parse(Console.ReadLine());
+//Console.Write("Matrix size: ");
+//int MATRIX_ROWS_COLUMNS = Int32.Parse(Console.ReadLine());
 
-Hardcoded h = new Hardcoded(MATRIX_ROWS_COLUMNS);
+//Hardcoded h = new Hardcoded(MATRIX_ROWS_COLUMNS);
 
-double[,] arrC = new double[MATRIX_ROWS_COLUMNS, MATRIX_ROWS_COLUMNS];
+//double[,] arrC = new double[MATRIX_ROWS_COLUMNS, MATRIX_ROWS_COLUMNS];
 
-for (int i = 0; i < MATRIX_ROWS_COLUMNS; i++)
-{
-    for (int j = 0; j < MATRIX_ROWS_COLUMNS; j++)
-    {
-        double input;
-        Console.Write("Enter value for ({0},{1}): ", i, j);
-        while (!double.TryParse(Console.ReadLine(), out input))
-        {
-            Console.Write("Enter correct value for ({0},{1}): ", i, j);
-        }
-        arrC[i, j] = input;
-    }
-}
+//for (int i = 0; i < MATRIX_ROWS_COLUMNS; i++)
+//{
+//    for (int j = 0; j < MATRIX_ROWS_COLUMNS; j++)
+//    {
+//        double input;
+//        Console.Write("Enter value for ({0},{1}): ", i, j);
+//        while (!double.TryParse(Console.ReadLine(), out input))
+//        {
+//            Console.Write("Enter correct value for ({0},{1}): ", i, j);
+//        }
+//        arrC[i, j] = input;
+//    }
+//}
 
-//Hardcoded h = new Hardcoded(4);
-//double[,] arrC = new double[,] { { 2, 7, 1, 2 }, { 3, -2, 8, 2 }, { 1, 5, 3, 2 }, { 2, 2, 2, 2 } };
+Hardcoded h = new Hardcoded(3);
+double[,] arrC = new double[,] { { 2, 7, 1 }, { 3, -2, 8 }, { 1, 5, 3 } };
 
 Console.WriteLine("C");
 Console.WriteLine("--------------------");
